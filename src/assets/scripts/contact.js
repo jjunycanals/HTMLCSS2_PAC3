62a8f575747180d78cdde9fcce806211478800aa
Jordi Juny <jjunyc@uoc.edu>
2024-05-31 11:50:40 +0200

    inici

---
+++ src/assets/scripts/contact.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelector("form").addEventListener("submit", function() {
+    window.location.href = "/thanks.html";
+  });
+});
---
+++ src/assets/scripts/data.js
@@ -0,0 +1,57 @@
+const chefs = [
+  {
+    "titol": "Xavier Pellicer",
+    "lloc": "Barcelona, Catalunya",
+    "detall": {
+      "text": "Xavier Pellicer és un reconegut cuiner català amb una llarga trajectòria en el món de la cuina. Destaca per la seva passió per els productes locals i de proximitat, i pel seu compromís amb una cuina sostenible i respectuosa amb el medi ambient.",
+      "multimedia": "xp.jpeg",
+      "sizemultimedia": []
+    }
+  },
+  {
+    "titol": "Carme Ruscalleda",
+    "lloc": "Sant Pol de Mar, Catalunya",
+    "detall": {
+      "text": "Carme Ruscalleda és una prestigiosa cuinera catalana amb estrella Michelin. Coneguda per la seva creativitat i per fusionar la tradició culinària catalana amb la innovació, ha portat la seva cuina a nivell internacional amb èxit.",
+      "multimedia": "cr5.webp",
+      "sizemultimedia": []
+    }
+  },
+  {
+    "titol": "Albert Adrià",
+    "lloc": "Barcelona, Catalunya",
+    "detall": {
+      "text": "Albert Adrià és un reconegut pastisser i cuiner català que ha revolucionat el món de la gastronomia amb les seves creacions innovadores. Amb una trajectòria consolidada al costat del seu germà Ferran Adrià, ha guanyat prestigi i reconeixement internacional.",
+      "multimedia": "ad4.png",
+      "sizemultimedia": []
+    }
+  },
+  {
+    "titol": "Nandu Jubany",
+    "lloc": "Vic, Catalunya",
+    "detall": {
+      "text": "Nandu Jubany és un reconegut xef català amb una estrella Michelin. La seva cuina destaca per la seva creativitat, la seva atenció als detalls i el seu amor per la tradició culinària catalana, que impregna tots els seus plats amb autenticitat i sabor.",
+      "multimedia": "nj2.png",
+      "sizemultimedia": []
+    }
+  },
+  {
+    "titol": "Ada Parellada",
+    "lloc": "Barcelona, Catalunya",
+    "detall": {
+      "text": "Ada Parellada és una cuinera i empresària catalana que ha revolucionat el panorama gastronòmic amb els seus restaurants i llibres de cuina. Apassionada defensora de la cuina casolana i de mercat, transmet autenticitat i calidesa en tots els seus plats.",
+      "multimedia": "avatar.png",
+      "sizemultimedia": []
+    }
+  },
+  {
+    "titol": "Víctor Quintillà",
+    "lloc": "Girona, Catalunya",
+    "detall": {
+      "text": "Víctor Quintillà és un jove i talentós cuiner català que ha destacat en el panorama gastronòmic amb la seva creativitat i passió per la cuina. Amb una trajectòria en restaurants de prestigi, porta una visió fresca i innovadora a tots els seus plats.",
+      "multimedia": "avatar.png",
+      "sizemultimedia": []
+    }
+  }
+]
+export default chefs;
